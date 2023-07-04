ee2923f2dd0373bf8dcb64b6034753d6dbccf2e5
TongSericus <tong_shen1@brown.edu>
2023-07-03 21:40:58 -0400

    removed the local measurement tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -475,39 +475,6 @@ end
     @. auxfield[1:idx_x-1, idx_t] = walker2.auxfield[1:idx_x-1, idx_t]
     r = compute_ratio_replica(extsys, qmc, auxfield, (idx_x, idx_t), walker1, ridx=2)
     @test r ≈ walker2.tmp_r[idx]
-
-    ### test the sweep with local measurements ###
-    # redefine walkers and replica
-    walker1 = HubbardWalker(system, qmc, φ₀)
-    walker2 = HubbardWalker(system, qmc, φ₀)
-    replica = Replica(extsys, walker1, walker2)
-    sweep!(system, qmc_nosave, replica, walker1, 1, loop_number=10, jumpReplica=true)
-    sweep!(system, qmc_nosave, replica, walker2, 2, loop_number=10, jumpReplica=true)
-    
-    sweep!_symmetric(system, qmc, replica, walker1, sampler, 1, collect(Θ+1:2Θ))
-    auxfield = copy(walker1.auxfield)
-    sweep!_symmetric(system, qmc, replica, walker1, sampler, 1, collect(Θ:-1:1))
-    # pick a random point in time [θ:2θ]
-    idx_t = rand(1:θ)
-    idx_x = rand(1:system.V)
-    idx = (θ - idx_t)*system.V + idx_x + div(system.V*system.L,2)
-    @. auxfield[:, idx_t+1:θ] = walker1.auxfield[:, idx_t+1:θ]
-    @. auxfield[1:idx_x-1, idx_t] = walker1.auxfield[1:idx_x-1, idx_t]
-    r = compute_ratio_replica(extsys, qmc, auxfield, (idx_x, idx_t), walker2, ridx=1)
-    @test r ≈ walker1.tmp_r[idx]
-
-    jump_replica!(replica, 1)
-    sweep!_symmetric(system, qmc, replica, walker2, sampler, 2, collect(Θ+1:2Θ))
-    auxfield = copy(walker2.auxfield)
-    sweep!_symmetric(system, qmc, replica, walker2, sampler, 2, collect(Θ:-1:1))
-    # pick a random point in time [θ:2θ]
-    idx_t = rand(1:θ)
-    idx_x = rand(1:system.V)
-    idx = (θ - idx_t)*system.V + idx_x + div(system.V*system.L,2)
-    @. auxfield[:, idx_t+1:θ] = walker2.auxfield[:, idx_t+1:θ]
-    @. auxfield[1:idx_x-1, idx_t] = walker2.auxfield[1:idx_x-1, idx_t]
-    r = compute_ratio_replica(extsys, qmc, auxfield, (idx_x, idx_t), walker1, ridx=2)
-    @test r ≈ walker2.tmp_r[idx]
 end
 
 # subsystem sampling test
@@ -574,25 +541,6 @@ end
     ### test the backward direction ###
     auxfield = copy(walker.auxfield)
     sweep!_symmetric(system, qmc, walker, collect(Θ:-1:1))
-    # pick a random point in time [θ:2θ]
-    idx_t = rand(1:θ)
-    idx_x = rand(1:system.V)
-    idx = (θ - idx_t)*system.V + idx_x + div(system.V*system.L,2)
-    @. auxfield[:, idx_t+1:θ] = walker.auxfield[:, idx_t+1:θ]
-    @. auxfield[1:idx_x-1, idx_t] = walker.auxfield[1:idx_x-1, idx_t]
-    # then create new test walkers
-    walker′ = HubbardSubsysWalker(extsys, qmc, φ₀, auxfield=auxfield)
-    auxfield[idx_x, idx_t] *= -1
-    walker″ = HubbardSubsysWalker(extsys, qmc, φ₀, auxfield=auxfield)
-
-    r = det(walker″.ImGA⁻¹[1]) / det(walker′.ImGA⁻¹[1])
-    @test r^2 ≈ walker.tmp_r[idx]
-
-    ### test the sweep with local measurements ###
-    walker = HubbardSubsysWalker(extsys, qmc, φ₀)
-    sweep!_symmetric(system, qmc, walker, collect(Θ+1:2Θ))
-    auxfield = copy(walker.auxfield)
-    sweep!_symmetric(system, qmc, walker, sampler, collect(Θ:-1:1))
     # pick a random point in time [θ:2θ]
     idx_t = rand(1:θ)
     idx_x = rand(1:system.V)
